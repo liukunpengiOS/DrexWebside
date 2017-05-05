e229d055df168ec45662d71bc519d94d7697e288
liukunpengiOS <1169405067@qq.com>
2017-05-05 09:24:27 +0800

    全球牧场编码更新1.0.1

--- globalMeadowLand.php
+++ globalMeadowLand.php
@@ -9,30 +9,14 @@
   <?php include 'head.html'; ?>
       <div style="text-align:center;position:relative;padding-top:60px;">
         <img src="images/globalRanch/1.jpg" style="width:100%;padding-bottom:auto;">
-        <div style="position:absolute;width:100%;height:auto;z-indent:2;top:200px;">
-          <h2 style="font-size:60px;color:#000;">全球健康食品服务商</h2>
-          <h2 style="font-size:38px;color:#000;">Global Healthy Food Supplier</h2>
+        <div style="position:absolute;width:100%;height:auto;z-indent:2;top:11em;">
+          <h2 style="font-size:3em;color:#000;">全球健康食品服务商</h2>
+          <h2 style="font-size:2em;color:#000;">Global Healthy Food Supplier</h2>
           <img src="images/globalRanch/ranchMap.png" style="width:100%;padding-bottom:auto;">
-          <h2 style="font-size:20px;">多尔克司全球牧场分布网络</h2>
-          <h2 style="font-size:10px;">DRex Global Ranch Distribution Network</h2>
+          <h2 style="font-size:1em;">多尔克司全球牧场分布网络</h2>
+          <h2 style="font-size:0.6em;">DRex Global Ranch Distribution Network</h2>
         </div>
       </div>
-      <!-- <div style="background: url(images/globalMeadowland/2.jpg) bottom;background-size:100% auto; width:100%;heihgt:0;padding-bottom:41.6%;position:relative;">
-      </div>
-      <div style="background: url(images/globalMeadowland/3.jpg) bottom;background-size:100% auto; width:100%;heihgt:0;padding-bottom:41.6%;position:relative;">
-      </div>
-      <div style="background: url(images/globalMeadowland/4.jpg) bottom;background-size:100% auto; width:100%;heihgt:0;padding-bottom:41.6%;position:relative;">
-      </div>
-      <div style="background: url(images/globalMeadowland/5.jpg) bottom;background-size:100% auto; width:100%;heihgt:0;padding-bottom:41.6%;position:relative;">
-      </div>
-      <div style="background: url(images/globalMeadowland/6.jpg) bottom;background-size:100% auto; width:100%;heihgt:0;padding-bottom:41.6%;position:relative;">
-      </div>
-      <div style="background: url(images/globalMeadowland/7.jpg) bottom;background-size:100% auto; width:100%;heihgt:0;padding-bottom:41.6%;position:relative;">
-      </div>
-      <div style="background: url(images/globalMeadowland/8.jpg) bottom;background-size:100% auto; width:100%;heihgt:0;padding-bottom:41.6%;position:relative;">
-      </div>
-      <div style="background: url(images/globalMeadowland/9.jpg) bottom;background-size:100% auto; width:100%;heihgt:0;padding-bottom:41.6%;position:relative;">
-      </div> -->
   <?php include 'footer.html'; ?>
   </body>
 </html>
